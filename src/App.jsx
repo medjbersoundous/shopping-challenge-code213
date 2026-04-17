c56e7664cb19a185fb011354da9dc49dae06f9fc
noursine <ssin44605@gmail.com>
2026-04-17 18:01:28 +0200

    add prestation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Home from "./pages/Home";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>this is react app for shopping challenge</h1>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Prestation from "../components/prestation";
+export default function Home() {
+  return (
+    <div>
+      <Prestation />
+    </div>
+  );
+}
